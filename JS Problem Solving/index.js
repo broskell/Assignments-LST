4aa56906c402239af96ef2ad6f3dd278719c28b2
Kellampalli Saathvik <saathvik.kp@gmail.com>
2025-10-25 20:24:29 +0530

    Completed Q.4 sum of odd nums

--- JS Problem Solving/index.js
+++ JS Problem Solving/index.js
@@ -1,3 +1,4 @@
+//Divisible numbers by 3 & 5 //
 function divisible_num(n) {
     let count = 0;
     for (let i = 1; i <= n; i++) {
@@ -9,3 +10,13 @@ function divisible_num(n) {
 }
 
 console.log(`No. of divisible numbers are: ${divisible_num(15)}`);
+
+// Sum of odd numbers using "n" //
+function sum_of_odd_nums(n) {
+    let count = 0;
+    for (let i = 0; i < n; i++) {
+        const odd_num = 2 * n + 1;
+        count += odd_num;
+    }
+    return count;
+}
